072f6d9c8117c1fb16de4c7871dfbb095ee4e3c3
gxs5493 <115658036+gxs5493@users.noreply.github.com>
2022-11-15 11:37:23 -0500

    Update minMaxRange.py

--- Motion_Detection_Project/3D Angles/minMaxRange.py
+++ Motion_Detection_Project/3D Angles/minMaxRange.py
@@ -20,7 +20,7 @@ import csv
 ## file_name is the file that contains angles for each frame
 angleTypes=["right_elbow_angle","right_shoulder_angle","right_wrist_angle","left_elbow_angle","left_shoulder_angle","left_wrist_angle"]
 for col in range(6): 
-    with open('elbow_1.csv') as csv_file:
+    with open('shoulder_1.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         #store [frame, angle] pairs
         list = []
@@ -48,19 +48,27 @@ for col in range(6):
         max_left_frame=-1
         start_left=-1
         start_left_frame=-1
+        astart_left=-1
+        astart_left_frame=-1
+        amin_left=-1
+        amin_left_frame=-1
         diff=0
         minCurRange=400
         for i in range(size):
             frame=list[i][0]
             angle=list[i][1]
             height=angle-minCurRange
-            if(min_left>angle):
+            if(angle==minAngle):
                 min_left=angle
                 min_left_frame=frame
             if((i==0 or list[i-1][1]<angle) and (i+1==len(list) or list[i+1][1]<angle)):
-                if(diff<height):
+                if(diff<height and angle==maxAngle):
                     max_left_frame=frame
-                    max_left=angle 
+                    max_left=angle
+                    astart_left=start_left
+                    astart_left_frame=start_left_frame
+                    amin_left=min_left
+                    amin_left_frame=min_left_frame
                 minCurRange=400
                 continue
 
@@ -76,6 +84,10 @@ for col in range(6):
         max_right_frame=-1
         start_right=-1
         start_right_frame=-1
+        astart_right=-1
+        astart_right_frame=-1
+        amin_right=-1
+        amin_right_frame=-1
         diff=0
         minCurRange=400
 
@@ -84,13 +96,17 @@ for col in range(6):
             frame=list[i][0]
             angle=list[i][1]
             height=angle-minCurRange
-            if(min_right>angle):
+            if(angle==minAngle):
                 min_right=angle
                 min_right_frame=frame
             if((i==0 or list[i-1][1]<angle) and (i+1==len(list) or list[i+1][1]<angle)):
-                if(diff<height):
+                if(diff<height and angle==maxAngle):
                     max_right_frame=frame
                     max_right=angle 
+                    astart_right=start_right
+                    astart_right_frame=start_right_frame
+                    amin_right=min_right
+                    amin_right_frame=min_right_frame
                 minCurRange=400
                 continue
                 
@@ -99,10 +115,20 @@ for col in range(6):
                 start_right_frame=frame
                 start_right=angle
 
-        if(min_left!=400 and min_left_frame!=-1 and max_left!=-400 and max_left_frame!=-1 and start_left!=-1 and start_left_frame!=-1):
-            print(f'\tRequired region for {angleTypes[col]} starts at coordinate: ({start_left_frame},{start_left}) and ends at ({max_left_frame},{max_left}) while the minimum Angle is at ({min_left_frame, min_left})')
+        print(f'\t minAngle:{minAngle} and maxAngle:{maxAngle}')
+        left=False
+        right=False
+        if(amin_left!=400 and amin_left_frame!=-1 and max_left!=-400 and max_left_frame!=-1 and astart_left!=-1 and astart_left_frame!=-1 and amin_right!=400 and amin_right_frame!=-1 and max_right!=-400 and max_right_frame!=-1 and astart_right!=-1 and astart_right_frame!=-1):
+            eps=0.0000001
+            left_diff=max_left-astart_left
+            right_diff=max_right-astart_right
+            if(left_diff-right_diff>=eps): left=True
+            else: right=True
+
+        if((amin_left!=400 and amin_left_frame!=-1 and max_left!=-400 and max_left_frame!=-1 and astart_left!=-1 and astart_left_frame!=-1) or left):
+            print(f'\tRequired- region for {angleTypes[col]} starts at coordinate: ({astart_left_frame},{astart_left}) and ends at ({max_left_frame},{max_left}) while the minimum Angle is at ({amin_left_frame, amin_left})')
         else: 
-            print(f'\tRequired region for {angleTypes[col]} starts at coordinate: ({start_right_frame},{start_right}) and ends at ({max_right_frame},{max_right}) while the minimum Angle is at ({min_right_frame, min_right})')
+            print(f'\tRequired# region for {angleTypes[col]} starts at coordinate: ({max_right_frame},{max_right}) and ends at ({astart_right_frame},{astart_right}) while the minimum Angle is at ({amin_right_frame, amin_right})')
 
 
 
